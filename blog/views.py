dc0fed32087c3f8703a88ba028ea1488d4e3cb81
Jacob Helsel <jacobnhelsel@gmail.com>
2021-04-06 00:15:18 -0500

    added functionality to blog page, added some polish, added static folder

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import Blog
 # Create your views here.
+theblogs = Blog.objects
+def allblogs(request):
+    return render(request, 'blog/allblogs.html', {'theblogs': theblogs})
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':detailblog})
